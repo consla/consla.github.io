f31eecb03564c364d0e5a59e02fa3254ffbc7382
consla <conslaaa@proton.me>
2023-11-27 23:28:26 -0900

    Quartz sync: Nov 27, 2023, 11:28 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -3,13 +3,13 @@ import * as Plugin from "./quartz/plugins"
 
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "consla",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
       provider: "plausible",
     },
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "consla.github.io",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
